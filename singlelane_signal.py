7d45ed00889c70f73404624d990fd56ba2f81545
jgraeb <jgraeben@caltech.edu>
2019-07-31 13:36:01 -0700

    fixed  singlelane_signal example

--- singlelane_signal.py
+++ singlelane_signal.py
@@ -3,7 +3,8 @@ from __future__ import print_function
 import logging
 
 from tulip import transys, spec, synth
-
+# get FSM package
+from tulip import dumpsmach
 
 logging.basicConfig(level=logging.WARNING)
 logging.getLogger('tulip.spec.lexyacc').setLevel(logging.WARNING)
@@ -25,8 +26,8 @@ sys.transitions.add_comb({'X3'}, {'X4', 'X2','X3'})
 sys.transitions.add_comb({'X4'}, {'X3','X4'})
 
 # Add atomic propositions to the states
-sys.atomic_propositions.add_from({'init', 'signal','goal'})
-sys.states.add('X0', ap={'init'})
+sys.atomic_propositions.add_from({'signal','goal'})
+#sys.states.add('X0', ap={'init'})
 sys.states.add('X2', ap={'signal'})
 sys.states.add('X4', ap={'goal'})
 
@@ -34,24 +35,33 @@ sys.states.add('X4', ap={'goal'})
 # sys.plot() 
 
 # @environ_section@
-env_vars = {'red','green'}
+env_vars = {'red'}      # light can be green or red
 env_init = set()                # empty set
-env_prog = {'red','green'}
-env_safe = {'((red && !green) || (green && !red))'}              # empty set
+env_prog = {'!red'}      # always eventually red, always eventually green
+env_safe = set()#{'((red && !green) || (green && !red))'}        # never green and red at the same time
 
 # System specification
 #
 # The system specification is that the car should drive to the goal
-#
-#     []<> goal && []((X2 && red) -> stay in state X2)
+# but only cross from cell 2 into cell 3 if the light is green
+#     []<> goal && []((in X2 && red) -> stay in state X2)
+
+#sys_vars = {'X0reach'}          # infer the rest from TS
+#sys_init = {'X0reach'}
+#sys_prog = {'home'}             # []<>home
+#sys_safe = {'(X (X0reach) <-> lot) || (X0reach && !park)'}
+#sys_prog |= {'X0reach'}
+
 
 # @specs_setup_section@
 # Augment the system description to make it GR(1)
-sys_vars = {'X0reach'}          # infer the rest from TS
-sys_init = {'X0reach'}          # initialized as true
+sys_vars = set()#{'X0reach'}          # infer the rest from TS
+sys_init = set()#{'X0reach'}          # initialized as true
 sys_prog = {'goal'}             # []<>goal
-sys_safe = {'(X (X0reach) <-> signal) || (X0reach && !red)'}#'X(X0reach)<->signal||(X0reach && !red)'}
-sys_prog |= {'X0reach'}
+sys_safe = {'((signal && red)->X(signal)) && (goal -> X(goal))'}#'X(X0reach)<->signal||(X0reach && !red)'}
+#sys_prog |= {'X0reach'}
+#sys_safe = {'(X (X0reach) <-> signal) || (X0reach && !red) && (goal -> X(goal))'}#'X(X0reach)<->signal||(X0reach && !red)'}
+
 
 # Create the specification
 specs = spec.GRSpec(env_vars, sys_vars, env_init, sys_init,
